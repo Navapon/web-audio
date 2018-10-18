3285adde435aa2361d237992f3e1c507df61c8e7
Navapon [PEE] <navapon@3dsinteractive.com>
2018-10-18 14:05:32 +0700

    test

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
+// binding this to that 
+const that = this;
+// initial value
 var recorder = "";
 var input = "";
-const that = this;
 var audio_context = "";
-var apiKey = "AIzaSyBb3gw82hDkzjZZGRkVz9Tw_MZnycJnekw"
+// body payload data
 var data = {
-  audio: {
-    content: null
-  },
-  config: {
-    encoding: "LINEAR16",
-    sampleRateHertz: 44100,
-    languageCode: "th-TH",
-    enable_word_time_offsets: false
-  }
+    "content": null, // base64 format
+    // "sample_rate": 44100, // sample rate value
+    "command": 'call_solr_group_category', // fix solr command
+    "language": 'th-TH'
 }
-var serverUrl = "localhost:8080";
+
+var serverUrl = "http://localhost:8080/speech-to-text";
 
 // Dedect client is support microphone and need a permission
 function requestUserPermission () {
   try {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     window.URL = window.URL || window.webkitURL;
 
     this.audio_context = new AudioContext();
@@ -32,63 +30,74 @@ function requestUserPermission () {
 }
 
 function startUserMedia() {
-  navigator.getUserMedia(
-    // constraints - only audio needed for this app
-    {
-      audio: true
-    },
-    function(stream) {
-      that.initialMedia(stream);
-    },
-    function(e) {
-      console.log("No live audio input: " + e);
-    }
-  );
+  navigator.mediaDevices.getUserMedia({audio: true})
+	.then(function(stream) {
+		that.initialMedia(stream);
+	})
+	.catch(function(err) {
+    console.log(err)
+	});
 }
 
 function initialMedia(stream) {
   // Media stream created
   input = this.audio_context.createMediaStreamSource(stream);
-  // Recorder initialised
+  // Recorder initialised need to set numChannel to 1 cause support only mono 
   this.recorder =  new Recorder(input,{ numChannels:1 });
 }
 
 function startRecording () { 
   this.audio_context.resume().then(() => {
     this.recorder.record();
-    console.log("Playback resumed successfully");
   })
 }
 
-function stopRecording() {
-  // Stopped Recording
-  this.recorder && this.recorder.stop();
+// just create a download link for ui
+function createDownloadLink(blob) {
+    var url = URL.createObjectURL(blob);
+    var li = document.createElement('li');
+    var au = document.createElement('audio');
+    var hf = document.createElement('a');
 
-  // create WAV download link using audio data blob
-  // this.processRecording();
-  recorder.clear();
+    au.controls = true;
+    au.src = url;
+    hf.href = url;
+    hf.download = new Date().toISOString() + '.wav';
+    hf.innerHTML = hf.download;
+    li.appendChild(au);
+    li.appendChild(hf);
+    recordingslist.appendChild(li);
 }
 
+//
 function processRecording() {
-  //check user device cause samplerate on each device is different
-  let sampleRate = that.detectDevice();
-  // this.data.audio.sampleRateHertz = sampleRate;
-  this.data.config.sampleRateHertz = sampleRate;
 
-  recorder && recorder.exportWAV(function(blob) {
-      var reader = new window.FileReader();
-      reader.readAsDataURL(blob);
-      reader.onloadend = function () {
-        const baseData = reader.result;
-        const base64Data = baseData.replace("data:audio/wav;base64,", "");
-        data.audio.content = base64Data;
-        //axios.post( serverUrl, data ) 
-        axios.post(`https://speech.googleapis.com/v1/speech:recognize?key=${apiKey}`, data)
-        .then(response => {
-          console.log(response)
-        })
-      };
-    });
+    this.recorder && this.recorder.stop();
+
+    //check user device cause sample rate on each device is different
+    let sampleRate = that.audio_context.sampleRate;
+    // this.data.sample_rate = sampleRate;
+    return new Promise(function(resolve,reject)  {
+      recorder && recorder.exportWAV(function(blob) {
+        var reader = new window.FileReader();
+        reader.readAsDataURL(blob);
+        // generate download link and player
+        this.createDownloadLink(blob)
+        reader.onloadend = function () {
+          const baseData = reader.result;
+          const base64Data = baseData.replace("data:audio/wav;base64,", "");
+          data.content = base64Data;
+          // post data to api
+          axios.post( serverUrl, data ) 
+          .then(response => {
+            if(response.data) {
+              resolve(response.data)
+              recorder.clear();
+            }
+          })
+        };
+      });
+    })
 }
 
 function detectDevice() {
@@ -107,8 +116,8 @@ function detectDevice() {
   }
 }
 
-
 var voiceSearchInstance;
+
 var VoiceSearch = {
   create: function () {
     if( voiceSearchInstance != null){
@@ -124,17 +133,15 @@ var VoiceSearch = {
       startRecording: function () {
         that.startRecording();
       },
-      stopRecording: function () {
-        that.stopRecording();
-      },
-      processRecording: function () {
-        that.processRecording()
+      stopAndProcessRecording: function () {
+        return new Promise(function(resolve,reject) {
+          that.processRecording().then(result => {
+            resolve(result)
+          })
+        })
       }
     };
-
     return voiceSearchInstance;
-
   }
-
 }
 
